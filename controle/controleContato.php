0c41f57be885e0f3e9d4f29d390ab901841f1a38
Luiz Reis <luizgabrieldosreisnunes2001@gmail.com>
2021-11-28 17:21:35 -0300

    controle dos botoes e das funções

---
+++ controle/controleContato.php
@@ -0,0 +1,24 @@
+<?php
+    include_once('../modelo/conexao.php');
+    include_once('../modelo/contatoDAO.php');
+
+    $conexao = new Conexao();
+    $conn = $conexao -> conectar();
+    $contatoDAO = new ContatoDAO();
+
+    if(isset($_POST["buscarEmail"])){
+        $contatoDAO -> buscarEmail($_POST["email"], $conn);
+    }
+
+    if(isset($_POST["buscarNome"])){
+        $contatoDAO -> buscarNome($_POST["nome"], $conn);
+    }
+
+    if(isset($_POST["listar"])){
+        $contatoDAO -> listar($conn);
+    }
+
+    if(isset($_POST["excluir"])){
+        $contatoDAO -> excluir($_POST["id"], $conn);
+    }
+?>
